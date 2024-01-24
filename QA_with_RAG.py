67f50d16e8fa3e4a892132c5f0f7120b04f71e4a
Chen Liang <cliang@xpanxion.com>
2024-01-24 14:42:31 -0600

    added chat history as context

--- QA_with_RAG.py
+++ QA_with_RAG.py
@@ -32,27 +32,84 @@ retriever = vectorstore.as_retriever()
 prompt = hub.pull("rlm/rag-prompt")
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
+# add the chat history into the structured prompt
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
+    ]
+)
+contextualize_q_chain = contextualize_q_prompt | llm | StrOutputParser()
+
+# format the question with the chat history
+from langchain_core.messages import AIMessage, HumanMessage
+
+contextualize_q_chain.invoke(
+    {
+        "chat_history": [
+            HumanMessage(content="What does LLM stand for?"),
+            AIMessage(content="Large language model"),
+        ],
+        "question": "What is meant by large",
+    }
+)
+
+# format the question with the context
+
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
+    ]
+)
+
+def contextualized_question(input: dict):
+    if input.get("chat_history"):
+        return contextualize_q_chain
+    else:
+        return input["question"]
+
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-# N-1: the last runnable in the chain
-rag_chain_from_docs = (
-    RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
-    | prompt
+
+rag_chain = (
+    RunnablePassthrough.assign(
+        context=contextualized_question | retriever | format_docs
+    )
+    | qa_prompt
     | llm
-    | StrOutputParser()
 )
 
-# N-2: use assign to pass the output to next runnable
-rag_chain_with_source = RunnableParallel(
-    {"context": retriever, "question": RunnablePassthrough()}
-).assign(answer=rag_chain_from_docs)
+chat_history = []
 
 # Ask a question 
-#answer = rag_chain_with_source.invoke("What is Task Decomposition?")
-answer = rag_chain_with_source.invoke("What does Agent do?")
-print(answer)
+question = "What is Task Decomposition?"
+ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
+chat_history.extend([HumanMessage(content=question), ai_msg])
+
+second_question = "What are common ways of doing it?"
+answer = rag_chain.invoke({"question": second_question, "chat_history": chat_history})
+chat_history.extend([HumanMessage(content=second_question), answer])
+
+print(chat_history)
 
 # cleanup
 vectorstore.delete_collection()
